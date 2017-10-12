6e23c7f16fe3726c7fef7ed123dd61a865bfc000
paras <mail2paras.s@gmail.com>
2017-10-12 22:01:36 +0530

    [RELEASE 1]

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,5 @@
 var api = {
-  prod_url: 'https://private-318dbc-eculum.apiary-mock.com/api/v1/',
-  url: 'http://192.168.43.95/api/v1/'
+  url: 'https://api.eculum.com/v1/'
 }
 
 export default api
